e9e6315fd9d7e76f138d989f86bf69bf68a51f1b
mirkone1994 <mirkochiappori@gmail.com>
2021-09-02 15:26:59 +0200

    api imported

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    musics: [],
+  },
   methods: {},
+  created(){
+    for(let i = 0; i < 1; i++) {
+    axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+        const music = res.data.response;
+        this.musics.push(music);
+    });
+  }
+},
 });
