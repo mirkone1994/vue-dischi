47bdab957b42d7b1317ae08b167cc39fcdcdc514
mirkone1994 <mirkochiappori@gmail.com>
2021-09-02 16:18:44 +0200

    exesrcise done, now bonuses

--- js/script.js
+++ js/script.js
@@ -9,11 +9,9 @@ const app = new Vue({
   },
   methods: {},
   created(){
-    for(let i = 0; i < 1; i++) {
     axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
         const music = res.data.response;
-        this.musics.push(music);
+        this.musics = music;
     });
-  }
 },
 });
