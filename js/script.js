f4d1f01f201603701638303d3b5e50f8a61f122c
mirkone1994 <mirkochiappori@gmail.com>
2021-09-03 10:35:00 +0200

    second bonus

--- js/script.js
+++ js/script.js
@@ -6,13 +6,28 @@ const app = new Vue({
   el: '#app',
   data: {
     musics: [],
+    selectedGenre: "All",
   },
   computed: {
     sortedMusics(){
       return this.musics.sort((a, b) => {
         return a.year - b.year;
       });
-    }
+    },
+    genresList(){
+      const genresList = [];
+      this.musics.forEach(music => {
+        if(!genresList.includes(music.genre))genresList.push(music.genre);                
+      });
+      return genresList;
+    },
+    filteredMusics(){
+      const musics = this.sortedMusics;
+      if(this.selectedGenre === 'All') return musics;
+      return musics.filter((music)=>{
+        return music.genre === this.selectedGenre;
+      })
+    },
   },
   methods: {},
   created(){
